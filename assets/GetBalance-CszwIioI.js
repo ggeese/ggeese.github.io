f5ea3d5d91528a6c72071f1d3c3a15538810a859
golden geese <comefy@gmail.com>
2024-10-22 13:45:14 -0500

    Add files via upload

---
+++ assets/GetBalance-CszwIioI.js
@@ -0,0 +1,1 @@
+import{u as s,f as c}from"./index-VJfl0Q39.js";const i=(t,e,n)=>{const{data:a,isError:o,isLoading:r}=s({address:e,token:t});return{balance:a?c(a.value.toString(),n):"0",isError:o,isLoading:r}};export{i as u};
